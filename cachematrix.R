a250a3789a928500b1d458f7f690cda5f45cd474
Гурин Константин <gurin@welcome-group.ru>
2016-01-23 23:23:18 +0400

    should work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The function takes as input a matrix. The data is cached.
+## Inside the function prescribed four functions
+## that allow you to access the data stored in the cache and change them.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  set_solve <- function(solve) m <<- solve
+  get_solve <- function() m
+  list(set = set, get = get,
+       set_solve = set_solve,
+       get_solve = get_solve)
 }
 
 
-## Write a short comment describing this function
+## The function takes as argument the result of
+## the previous solve for matrix and calculates if the calculation
+## has not been made previously. Otherwise it returns the value
+## of the calculated earlier for the same array of cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$get_solve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$set_solve(m)
+  m
 }
